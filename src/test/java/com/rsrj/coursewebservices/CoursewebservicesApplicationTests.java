3802f704f885d4d2e6e7c0d7135062a7a25ae35b
rsrj <rocha.railton13@gmail.com>
2021-10-22 11:30:34 -0300

    Project Created

---
+++ src/test/java/com/rsrj/coursewebservices/CoursewebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsrj.coursewebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursewebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
